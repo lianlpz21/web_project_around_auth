8bedfc8389aa5e2719abed6f82482d6a5878f573
lianlpz21 <lian.lpz21@gmail.com>
2025-01-20 21:19:47 -0300

    header close sesion ready

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,17 @@ import logo from "../images/logo.svg";
 export default function Header({ userEmail, onLogout, isAuthenticated }) {
   return (
     <header className="header">
-      <img src={logo} alt="logo Around the U.S." className="header__logo" />
-      {isAuthenticated && (
-        <ul className="header__ul">
-          <li className="header__list">
+      <div className="header__top">
+        <img src={logo} alt="logo Around the U.S." className="header__logo" />
+        {isAuthenticated && (
+          <div className="header__user-info">
             <h3 className="header__user-email">{userEmail}</h3>
-          </li>
-          <li className="header__list">
             <button className="header__logout-btn" onClick={onLogout}>
               Cerrar Sesión
             </button>
-          </li>
-        </ul>
-      )}
+          </div>
+        )}
+      </div>
       <span className="header__line"></span>
     </header>
   );
